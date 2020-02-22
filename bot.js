f9d348d108f1198528a131cd34dfe11633595742
Alex Halibut <anhromad@mtu.edu>
2020-02-21 22:06:50 -0500

    This damn goose can bite me

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-const { token } = require('./secret.json');
+const {token} = require('./secret.json');
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
@@ -6,24 +6,49 @@ client.on("ready", () => {
     console.log("I am ready!");
 });
 
+// .startsWith = commands
+// .includes = memes to post if someone says keyword in message
 client.on("message", (message) => {
     if (message.content.startsWith("ping")) {
         message.channel.send("pong!");
     }
+
     if (message.content.startsWith(",help")) {
-        message.channel.send("Commands: ping, BOG");
+        message.channel.send("Commands: ping");
     }
 
-    if (message.content.includes("BOG")){
+    if (message.content.includes("BOG")) {
         message.channel.send("COME BACK FORSAN :MEGALUL:");
     }
 
-    if(message.content.includes("homeless")){
+    if (message.content.includes("homeless")) {
         const house = new Discord.Attachment('https://i.imgur.com/DkoestG.png');
 
         message.channel.send(house);
         message.channel.send("JUST GET A HOUSE")
     }
+
+    if(message.content.includes("peace")){
+        const goose_knife = new Discord.Attachment('https://i.imgur.com/sf0o3zI.png');
+
+        message.channel.send(goose_knife);
+        message.channel.send("Peace was never an option.");
+    }
+
+
 });
 
 client.login(token);
+
+/**
+ * Function to return a random
+ * @param min
+ * @param max
+ * @returns {*}
+ */
+function getRandomInt(min, max){
+    min = Math.floor(min);
+    max = Math.ceil(max);
+
+    return Math.floor(Math.random() * (max - min)); //The maximum is exclusive and the minimum is exclusive
+}
